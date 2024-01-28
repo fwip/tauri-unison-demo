e9f564c4210dc5e5b28c820ab46315aaa3357acd
Jemma Nelson <jemma.s.nelson@gmail.com>
2024-01-28 15:42:59 -0500

    Make splashscreen work

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,98 +4,64 @@
 use tauri::api::process::{Command,CommandEvent};
 use tokio::time::{sleep, Duration};
 
+// The time in-between requests when checking to see if the TV server is up
+const SERVER_POLL_INTERVAL : Duration = Duration::from_millis(100);
+// The max number of requests to make.
+// TODO: Have some logic for dealing with timeout.
+const SERVER_POLL_REQUESTS: usize = 100;
+// An additional duration to wait before dismissing splash screen, to allow the TV app time to
+// initialize and populate the UI.
+const ADDITIONAL_SPLASH_DURATION: Duration = Duration::from_millis(1000);
 
-//#[tauri::command]
-//async fn launch(name: &str) -> Result<String, ()> {
-//    let url = "http://localhost:8080".to_owned();
-//    let ucm = Command::new_sidecar("ucm")
-//        .expect("Failed to create `ucm` binary command (possible bundling issue?)")
-//        .args(&["run.compiled", &("resources/".to_owned() + name)]);
-//
-//    let (mut rx, mut child) = ucm.spawn().expect("Failed to spawn command");
-//    println!("Spawned {}", name);
-//    tauri::async_runtime::spawn(async move {
-//        while let Some(event) = rx.recv().await {
-//            match event {
-//                CommandEvent::Stderr(s) => println!("Stderr: {}", s),
-//                CommandEvent::Stdout(s) => println!("StdOut: {}", s),
-//                CommandEvent::Error(e) => println!("Error: {}", e),
-//                CommandEvent::Terminated(_) => println!("Terminated"),
-//                ce => println!("Some other command event: {:?}", ce),
-//            };
-//        }
-//    });
-//    let mut i = 0;
-//    while i < 10 { 
-//        let response = reqwest::get(url.clone()).await;
-//        match response {
-//            Ok(response) => {
-//                println!("Ok! {:?}", response);
-//                break
-//            },
-//            Err(e) => (), //println!("Err: {}", e),
-//        }
-//        i+=1;
-//        sleep(Duration::from_millis(100)).await;
-//
-//    }
-//
-//    Ok(url)
-//}
 
 fn main() {
     use tauri::Manager;
 
-
     tauri::Builder::default()
         .plugin(tauri_plugin_websocket::init())
-        //.invoke_handler(tauri::generate_handler![launch])
         .setup(|app| {
-            let name = "main";
+            let name = "main.uc";
 
             let main_window = app.get_window("main").unwrap();
             let splashscreen_window = app.get_window("splashscreen").unwrap();
+            let url = "http://localhost:8080".to_owned();
 
             tauri::async_runtime::spawn(async move {
-                let url = "http://localhost:8080".to_owned();
                 let ucm = Command::new_sidecar("ucm")
                     .expect("Failed to create `ucm` binary command (possible bundling issue?)")
                     .args(&["run.compiled", &("resources/".to_owned() + name)]);
-                let (mut rx, mut child) = ucm.spawn().expect("Failed to spawn command");
-                while let Some(event) = rx.recv().await {
-                    match event {
-                        CommandEvent::Stderr(s) => println!("Stderr: {}", s),
-                        CommandEvent::Stdout(s) => println!("StdOut: {}", s),
-                        CommandEvent::Error(e) => println!("Error: {}", e),
-                        CommandEvent::Terminated(_) => println!("Terminated"),
-                        ce => println!("Some other command event: {:?}", ce),
-                    };
-                }
+                let (mut rx, _child) = ucm.spawn().expect("Failed to spawn command");
+                tauri::async_runtime::spawn(async move {
+                    while let Some(event) = rx.recv().await {
+                        match event {
+                            CommandEvent::Stderr(s) => println!("Stderr: {}", s),
+                            CommandEvent::Stdout(s) => println!("StdOut: {}", s),
+                            CommandEvent::Error(e) => println!("Error: {}", e),
+                            CommandEvent::Terminated(t) => println!("Terminated: {:?}", t),
+                            ce => println!("Some other command event: {:?}", ce),
+                        };
+                    }
+                });
                 let mut i = 0;
-                while i < 10 { 
+                while i < SERVER_POLL_REQUESTS { 
                     let response = reqwest::get(url.clone()).await;
                     match response {
-                        Ok(response) => {
-                            println!("Ok! {:?}", response);
+                        Ok(_response) => {
                             break
                         },
-                        Err(e) => (), //println!("Err: {}", e),
+                        Err(_e) => (), //println!("Err: {}", e),
                     }
                     i+=1;
-                    sleep(Duration::from_millis(100)).await;
+                    sleep(SERVER_POLL_INTERVAL).await;
                 }
-                main_window.eval(&format!("location.href = {url};").to_owned()).expect("could not eval JS");
-                sleep(Duration::from_millis(100)).await;
+                main_window.eval(&format!("window.location.href = '{url}';").to_owned()).expect("could not eval JS");
+                sleep(ADDITIONAL_SPLASH_DURATION).await;
                 splashscreen_window.close().unwrap();
                 main_window.show().unwrap();
             });
-
 
             Ok(())
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-
-    //child.kill().expect("Could not kill ucm child process");
-
 }
