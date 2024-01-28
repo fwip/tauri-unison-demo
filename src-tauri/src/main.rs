630018a95d22aa925a68e3cfdfe37d31a31a3697
Jemma Nelson <jemma.s.nelson@gmail.com>
2024-01-28 17:26:30 -0500

    Little bit of refactoring

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::error::Error;
+use tauri::Manager;
 use tauri::api::process::{Command,CommandEvent};
 use tokio::time::{sleep, Duration};
 
@@ -47,52 +49,53 @@ async fn simple_ucm_monitor(rx: &mut tauri::async_runtime::Receiver<CommandEvent
     }
 }
 
-fn main() {
-    use tauri::Manager;
+fn setup_ucm(filename: String, app: &mut tauri::App) -> Result<(), Box<dyn Error>> {
 
+    let url = "http://localhost:8080".to_owned();
+
+    let main_window = app.get_window("main").unwrap();
+    let splashscreen_window = app.get_window("splashscreen").unwrap();
+
+    tauri::async_runtime::spawn(async move {
+        // Run Unison server
+        let ucm = Command::new_sidecar("ucm")
+            .expect("Failed to create `ucm` binary command (possible bundling issue?)")
+            .args(&["run.compiled", &("resources/".to_owned() + &filename)]);
+        let (mut rx, _child) = ucm.spawn().expect("Failed to spawn command");
+
+        // Simple monitoring of the UCM process
+        tauri::async_runtime::spawn(async move {
+            simple_ucm_monitor(&mut rx).await;
+        });
+
+        // Wait until server is running
+        match wait_until_server_is_up(url.clone()).await {
+            Ok(_response) => {
+                // Redirect to the local server, and throw away the splash screen.
+                main_window.eval(&format!("window.location.href = '{url}';").to_owned()).expect("could not eval JS");
+                sleep(ADDITIONAL_SPLASH_DURATION).await;
+                splashscreen_window.close().unwrap();
+                main_window.show().unwrap();
+            },
+            Err(e) => {
+                println!("Error: {e}");
+                //child.kill().expect("destroying ucm process");
+                main_window.eval(&format!("window.location.href = 'error.html';").to_owned()).expect("could not eval JS");
+                sleep(ADDITIONAL_SPLASH_DURATION).await;
+                splashscreen_window.close().unwrap();
+                main_window.eval(&format!("error('{e}');")).expect("eval error JS");
+                main_window.show().unwrap();
+            }
+        }
+    });
+    Ok(())
+}
+
+fn main() {
+    let filename = "main.uc".to_owned();
     tauri::Builder::default()
         .plugin(tauri_plugin_websocket::init())
-        .setup(|app| {
-            let name = "main.uc";
-            let url = "http://localhost:8080".to_owned();
-
-            let main_window = app.get_window("main").unwrap();
-            let splashscreen_window = app.get_window("splashscreen").unwrap();
-
-            tauri::async_runtime::spawn(async move {
-                // Run Unison server
-                let ucm = Command::new_sidecar("ucm")
-                    .expect("Failed to create `ucm` binary command (possible bundling issue?)")
-                    .args(&["run.compiled", &("resources/".to_owned() + name)]);
-                let (mut rx, _child) = ucm.spawn().expect("Failed to spawn command");
-
-                // Simple monitoring of the UCM process
-                tauri::async_runtime::spawn(async move {
-                    simple_ucm_monitor(&mut rx).await;
-                });
-
-                // Wait until server is running
-                match wait_until_server_is_up(url.clone()).await {
-                    Ok(_response) => {
-                        // Redirect to the local server, and throw away the splash screen.
-                        main_window.eval(&format!("window.location.href = '{url}';").to_owned()).expect("could not eval JS");
-                        sleep(ADDITIONAL_SPLASH_DURATION).await;
-                        splashscreen_window.close().unwrap();
-                        main_window.show().unwrap();
-                    },
-                    Err(e) => {
-                        println!("Error: {e}");
-                        //child.kill().expect("destroying ucm process");
-                        main_window.eval(&format!("window.location.href = 'error.html';").to_owned()).expect("could not eval JS");
-                        sleep(ADDITIONAL_SPLASH_DURATION).await;
-                        splashscreen_window.close().unwrap();
-                        main_window.eval(&format!("error('{e}');")).expect("eval error JS");
-                        main_window.show().unwrap();
-                    }
-                }
-            });
-            Ok(())
-        })
+        .setup(|app| { setup_ucm(filename, app) } )
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
